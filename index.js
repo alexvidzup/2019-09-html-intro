84f3c90e1e63f71e3ce783f5a90b58de50951763
Aleksandr Vidzup <AVidzup@spartaglobal.com>
2019-09-13 16:39:07 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+/* going to write some incredible NODE */
+console.log('this is a node application and next week we are gonna build an amazing game')
+var i = 0;
+while (true){
+	i++
+	console.log(i)
+	if (i>10)break;
+}
---
+++ web-server-01.js
@@ -0,0 +1,12 @@
+var http = require('http');
+console.log('about to build a web server');
+var counter = 0;
+http.createServer(function(request,response){
+	counter++;
+	console.log('request has come in')
+	response.writeHead(200,{
+		'Content-Type':'text/plain'
+	})
+	response.end('Server responding = ' + counter + ' requests')
+
+}).listen(8080,'127.0.0.1')
